5eb0834e26d78a94508f36df90d0b9bce9a88973
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-02-03 22:14:22 +0545

    Time: 1 ms (89.78%) | Memory: 51.6 MB (45.03%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let pairHashMap = { "(":")", "{":"}", "[":"]" };
+
+    for(let i = 0;i<s.length; i++){
+        let char = s[i];
+        if(pairHashMap[char]){
+            stack.push(char);
+        } else if (pairHashMap[stack.pop()] !== char) {
+            return false
+        }
+    }
+
+    return stack.length === 0;
+};
