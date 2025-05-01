47b0c3154174fb4ec04eb4fdc941220d6c4654ec
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-01 11:59:16 +0545

    Time: 4 ms (89.78%) | Memory: 59.1 MB (46.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let longestStr = 0;
-    let set = new Set();
+   let longestStr = 0;
+   let set = new Set();
 
-    let left = 0;
-    let right = 0;
-    
-    while(right < s.length){
-        let letter = s[right];
+   let left = 0;
+   let right = 0;
 
-        if(!set.has(letter)){
-            set.add(letter)
-            longestStr = Math.max(longestStr, set.size)
-            right++;
-        } else {
-            set.delete(s[left]);
-            left++
-        }
+   while(right < s.length){
+    let letter = s[right];
+
+    if(!set.has(letter)){
+        set.add(letter);
+        longestStr = Math.max(longestStr, set.size);
+        right++;
+    } else {
+        set.delete(s[left]);
+        left++;
     }
-    return longestStr;
+   }
+   return longestStr;
 };
