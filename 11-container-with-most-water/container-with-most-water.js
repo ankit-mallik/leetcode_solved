fbcffd0ee29f88c76dda220d6f9aeb02a2f42c4a
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-02 12:31:30 +0545

    Time: 3 ms (55.28%) | Memory: 63.6 MB (17.10%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length -1;
+    let maxima =0;
+
+    while(left < right){
+
+        let width = right- left;
+        let maxArea = Math.min(height[left], height[right]) * width;
+
+        maxima = Math.max(maxArea, maxima);
+
+        if(height[left] < height[right]){
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxima;
+};
