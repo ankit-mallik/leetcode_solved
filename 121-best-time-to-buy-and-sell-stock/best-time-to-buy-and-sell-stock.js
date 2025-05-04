a27ed90aa220f116e7bfb2815caf5441c4fc1b08
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-04 12:29:59 +0545

    Time: 1 ms (96.81%) | Memory: 65.1 MB (56.70%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = Infinity;
+    let minPrice = prices[0];
     let maxProfit = 0;
     for(let i=0 ; i < prices.length; i++){
         if(prices[i] < minPrice){
             minPrice = prices[i]
-        } else {
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice)
         }
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice)
     }
     return maxProfit;
 };
