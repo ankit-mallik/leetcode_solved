dd84a1fbe5a412185194488eb652c4caf66238cb
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-08 11:08:59 +0545

    Time: 1 ms (10.79%) | Memory: 58.5 MB (6.91%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,10 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i= 0; i<nums.length;i++){
-        if(nums[i] === target){
-            return i;
+    let left = 0
+    let right = nums.length - 1
+
+    while(left<=right){
+        mid = Math.floor((left + right)/2);
+        if(nums[mid] === target){
+            return mid;
+        } else if(target > nums[mid]){
+            left = left + 1;
+        } else if (target < nums[mid]){
+            right = right - 1;
         }
+
     }
-    return -1;
+    return -1
 };
