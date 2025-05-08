bbe5316d6c1ac054ccce096d97e35ca2c060161f
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-08 10:24:29 +0545

    Time: 4 ms (33.69%) | Memory: 57.6 MB (87.16%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -8,7 +8,7 @@ var minSubArrayLen = function(target, nums) {
     let left = 0;
     let sum = 0;
 
-    for (let i = 0; i<=nums.length; i++){
+    for (let i = 0; i<nums.length; i++){
         sum += nums[i];
         while(sum >= target){
             minLength = Math.min(minLength, i - left + 1)
