aad438dddb60b6dabb89e9184de8b0373ed02403
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-05-08 10:22:46 +0545

    Time: 5 ms (16.96%) | Memory: 57.7 MB (79.43%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let minLength = Infinity;
+    let left = 0;
+    let sum = 0;
+
+    for (let i = 0; i<=nums.length; i++){
+        sum += nums[i];
+        while(sum >= target){
+            minLength = Math.min(minLength, i - left + 1)
+            sum -= nums[left++]
+        }
+    }
+    return minLength === Infinity? 0 : minLength
+};
