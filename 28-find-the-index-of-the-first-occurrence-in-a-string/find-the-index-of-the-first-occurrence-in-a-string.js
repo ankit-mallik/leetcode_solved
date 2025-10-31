4f770eef707df40cfbfad85cf6e86f486c824514
ankit-mallik <59758989+ankit-mallik@users.noreply.github.com>
2025-10-31 09:13:19 +0545

    Time: 0 ms (100.00%) | Memory: 54 MB (60.19%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length > haystack.length) return -1
+
+    for(let i = 0; i < haystack.length; i ++){
+        if(needle[0] === haystack[i]){
+            if(haystack.substring(i, i + needle.length) === needle)
+                return i;
+        }
+    }
+    return -1
+};
